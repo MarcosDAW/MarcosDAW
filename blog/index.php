cb4f6930b5ef0f7fbb7f8b394a1fdb4bf8b6e013
MarcosDAW <markos_2008_15@hotmail.com>
2021-10-04 18:44:18 +0200

    clase de entrar usuario y borrar

--- blog/index.php
+++ blog/index.php
@@ -8,17 +8,27 @@
     <title>Document</title>
 </head>
 <?php 
+    //guarda todo lo que hace el usuario
+    //es una variable de estado 
+    
+    session_start();
+
+    if(!isset($_SESSION['loged'])){
+        $_SESSION['loged']=false;
+        echo "creo loged: ";
+    }
+
+    echo "antes de logear: ".var_dump($_SESSION['loged']);
+
     $mysql = mysqli_connect("localhost", "root", "", "blog");
     if($mysql->connect_error){
         echo "Error al conectar";
     }else{
         echo 'conectado <br>';
     }
-    
-    /*
-    
-    */
-    
+
+    $loged=false;
+
     /*
     if(isset($_POST['insertar'])){
         $sql = "INSERT INTO articulos (titulo,texto,autor,fecha) VALUES (
@@ -30,59 +40,85 @@
         echo "no hay datos ingresados";
     }
     */
-    echo'<button title="loguear">Log In</button>';
 
     if(isset($_POST['usuario']) && isset($_POST['contraseña'])){
-        $usuario = $_POST['usuario'];
-        $contraseña = $_POST['contraseña'];
-        $sql = "SELECT nombre_usuario,contraseña FROM usuarios WHERE nombre_usuario = '$usuario' AND contraseña = '$contraseña'";
-        $result = mysqli_query($mysql,$sql);
-        if($result){
-            if($row = mysqli_fetch_row($result)){
-                $sql = "SELECT titulo,texto,autor,fecha,imagen FROM articulos";
-                $result = mysqli_query($mysql,$sql);
-                echo '<button title="agregar_articulo">Articulo Nuevo</button>';
-                foreach($result as $rows){
-                    echo '<h1>'.$rows['titulo'].'</h1>';
+         
+         $usuario =  $_POST['usuario'];
+         $contraseña =  $_POST['contraseña'];
+         $sql = "SELECT nombre_usuario,contraseña FROM usuarios WHERE nombre_usuario = '$usuario' AND contraseña = '$contraseña'";
+         $resultado = mysqli_query($mysql,$sql);
+        if($resultado){
+            if($row = mysqli_fetch_row($resultado)){
+                 $_SESSION['loged']=true;
+                 $_SESSION['usuario'] =  $_POST['usuario'];
+                 echo "despues de logear: ".var_dump($_SESSION['loged']);
+                 echo "<a href= 'index.php?logout'>Cerrar Sesión</a> ";
+                 $sql = "SELECT titulo,texto,autor,fecha,imagen FROM articulos";
+                 $resultado = mysqli_query($mysql,$sql);
+                
+                foreach($resultado as  $filas){
+                    echo '<h1>'.  $filas['titulo']. '</h1>';
                     echo '<div>';
-                    echo '<p>'.$rows['texto'].'</p>';
-                    echo '<p>'.$rows['autor'].'</p>';
-                    echo '<p>'.$rows['fecha'].'</p>';
-                    echo '<img width=200px src="'.$rows['imagen'].'">';
+                    echo '<p>'.  $filas['texto'].'</p>';
+                    echo '<p>'.  $filas['autor'].'</p>';
+                    echo '<p>'.  $filas['fecha'].'</p>';
+                    echo '<img width=200px src="'.  $filas['imagen'].'">';
                     echo '<hr>';
                     echo '</div>';
+                    if( $_SESSION[ 'usuario' ] ==  $filas[ 'autor' ]){
+                        
+                        echo "< a href='index.php?eliminar'>Eliminar</a>";
+                        if( isset($_GET['eliminar']) ){
+                            $usuarioLogeado = $_SESSION['usuario'];
+                            $sql = "DELETE FROM articulos WHERE autor = "'.$usuarioLogeado.';
+                            $result = mysqli_query($mysql,$sql);
+                            $row = mysqli_fetch_row($result);
+                        }
+                    }
                 }  
+
                 
+
+                echo "<form action='index.php' method='POST'>";
+                echo "input type='text' name='titulo' placeholder='Titulo'>";
+                echo "<br>";
+                echo "<input type='text' name='texto' placeholder='Texto'>";
+                echo "<br>";
+                echo "<input type='text' name='autor' placeholder='Autor'>";
+                echo "<br>";
+                echo "<input type='text' name='fecha' placeholder='00-00-00 00:00:00'>";
+                echo "<br>";
+                echo "<input type='submit' value='insertar'>";
+                echo "</form>";
             }else{
                 echo "usuario incorrecto";
             }
         }
     } 
-
+    
 ?>
+<?php
+echo "antes de decir hola: ".var_dump($_SESSION['loged']);
+
+if($_SESSION['loged']){
+    echo "Hola ".$_SESSION['usuario'];
+}else{
+    echo "no estas logeado";
+}
+    
+?>
+
 <body>
     <form action="index.php" method="POST">
         <input type="text" name="usuario" placeholder="Usuario">
         <br>
         <input type="text" name="contraseña" placeholder="Contraseña">
         <br>
-        <input type="submit" name="ingresar" placeholder="Ingresar">
+        <input type="submit" value="Iniciar Sesión">
     </form>
 
-    <form action="index.php" method="POST">
-        <input type="text" name="titulo" placeholder="Titulo">
-        <br>
-        <input type="text" name="texto" placeholder="Texto">
-        <br>
-        <input type="text" name="autor" placeholder="Autor">
-        <br>
-        <input type="text" name="fecha" placeholder="00-00-00 00:00:00">
-        <br>
-        <input type="submit" name="insertar" placeholder="Insertar">
-    </form>
-    <form action="index.php" method="POST">
-        
-    </form>
+    
+
 
 </body>
 </html>
